38b815657229d141e470f1cc5056e51d446184c7
Kevin <kevin@abouhanna.com>
2025-04-24 03:33:12 +0300

    me7en

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
-import { ProductProvider } from "./contexts/ProductContext.tsx";
+import { ProductProvider } from "./contexts/ProductContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
